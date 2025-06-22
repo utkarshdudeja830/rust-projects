630de32aa65e32b19a8168624020b845cd4438b8
Utkarsh Dudeja <dudeja.utkarsh@gmail.com>
2025-06-22 20:48:25 +0530

    Port sniffer updated for 1 ip

--- 6-port_sniffer_cli/src/main.rs
+++ 6-port_sniffer_cli/src/main.rs
@@ -1,18 +1,23 @@
 use std::env;
-use std::net::TcpStream
-const MAX: u16 = 65535;
+use std::net::TcpStream;
+const MAX: u16 = 65335;
 
-fn main(){
-    let args Vec<String> = env::args().collect();
-    let mut v = Vec::new();
-
-    let ip_address = args[0].clone();
-    for port in 0..MAX{
-
-        let mut stream = TcpStream::connect("{ip_address}:{port}")?;
-
-        stream.write(&[1])?;
-        stream.read(&mut [0; 128])?;
-        Ok(())
+fn scan(ip_address: &str, port: u16) -> bool{
+    match TcpStream::connect(format!("{}:{}", ip_address ,port)){
+        Ok(_) => true,
+        Err(_) => false,
     }
+}
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    // let mut v = Vec::new();
+
+    let ip_address = args[1].clone();
+
+    for port in 0..MAX{
+        if scan(&ip_address, port) {
+            println!("Port {}: Open", port);
+        }
+    }
+
 }
